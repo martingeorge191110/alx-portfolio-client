f500c8a5717cb7598f7fed10df790d4a04ecf27b
Martin George <martingeorge191110@gmail.com>
2025-03-05 22:33:58 +0200

    adjust some sytnaxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Register from './pages/auth/register';
+import Login from './pages/auth/login';
+
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route exact path='register' element={<Register/>}/>
+        <Route exact path='login' element={<Login/>}/>
+      </Routes>
     </>
   )
 }
