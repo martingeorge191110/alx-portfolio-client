047794660523e9207fa803ea7b29347fb9b63af8
Martin George <martingeorge191110@gmail.com>
2025-03-22 16:55:48 +0200

    Update VITE config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'alx-portfolio-client',
+  // base: 'alx-portfolio-client',
   build: {
     rollupOptions: {
       output: {
